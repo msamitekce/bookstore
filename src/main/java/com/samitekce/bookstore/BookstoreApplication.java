06dfb7789a2a71ce628a0b9f6d361101d88077d0
msamitekce <msamitekce@gmail.com>
2021-03-20 00:33:19 +0200

    heroku att 6

--- src/main/java/com/samitekce/bookstore/BookstoreApplication.java
+++ src/main/java/com/samitekce/bookstore/BookstoreApplication.java
@@ -23,6 +23,8 @@ public class BookstoreApplication {
     public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository,
 	    UserRepository urepository) {
 	return (args) -> {
+	    urepository.deleteAll();
+	    crepository.deleteAll();
 	    crepository.save(new Category("IT"));
 	    crepository.save(new Category("Business"));
 	    crepository.save(new Category("Law"));
@@ -33,9 +35,10 @@ public class BookstoreApplication {
 	    repository.save(new Book("Best Book 3", "John A", "2012", "isbn33", "50.99",
 		    crepository.findByName("Business").get(0)));
 
-	    User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6","user@user.com", "USER");
-	    User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C","admin@admin.com", "ADMIN");
-	    urepository.deleteAll();
+	    User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6",
+		    "user@user.com", "USER");
+	    User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
+		    "admin@admin.com", "ADMIN");
 	    urepository.save(user1);
 	    urepository.save(user2);
 	};
